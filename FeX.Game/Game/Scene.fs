5045972b54e95f59d0b1d6eb4e67dfee010f1c04
Angel D. Munoz <scythes.lol@gmail.com>
2020-08-11 18:23:42 -0600

    experiment with a sample scene and a simple object

--- FeX.Game/Game/Scene.fs
+++ FeX.Game/Game/Scene.fs
@@ -2,29 +2,57 @@
 
 open System
 open Microsoft.Xna.Framework
-
+open Microsoft.Xna.Framework.Graphics
+open Types
+open Microsoft.Xna.Framework.Content
 
 type Scene =
-    { name: string }
-
+    { name: string
+      spriteBatch: Option<SpriteBatch>
+      gameObjects: list<GameObject>
+    }
 
 module Scene =
 
-    let init () =
+    let init (scene: Scene) : Scene =
         // Add Initialization code for the scene
-        raise (NotImplementedException "Scene init not implemented")
+        scene
 
-    let load () =
+    let load (spriteBatch: SpriteBatch) (content: ContentManager) (scene: Scene) : Scene =
         // Load content, sprites whatsovever
-        raise (NotImplementedException "Scene load not implemented")
+        let objects = 
+            if scene.name = "Sample Scene" then 
+                let player : Player =
+                    let texture = content.Load<Texture2D>("ball")
+                    { texture = texture
+                      speed = 250.f
+                      position = Vector2.Zero
+                      playerIndex = PlayerIndex.One
+                      bounds = Rectangle(Vector2.Zero.ToPoint(), texture.Bounds.Size) }
+                [player :> GameObject]
+            else 
+                scene.gameObjects
 
-    let update (gametime: GameTime) = 
+        let gameObjects = 
+             objects |> List.map (fun go -> go.Load content)
+        { scene with spriteBatch = Some spriteBatch; gameObjects = gameObjects }
+
+    let update (gametime: GameTime) (scene: Scene) : Scene = 
         // Update logic
-        raise (NotImplementedException "Scene update not implemented") 
+        let gameObjects = scene.gameObjects |> List.map (fun go -> go.Update gametime)
+        { scene with gameObjects = gameObjects }
 
-    let draw (gametime: GameTime) =
+    let draw (gametime: GameTime) (scene: Scene) : Scene =
         // Draw stuff on screen
-        raise (NotImplementedException "Scene draw not implemented")    
+        match scene.spriteBatch with 
+        | Some batch ->
+            batch.Begin()
+            scene.gameObjects 
+            |> List.iter (fun go -> go.Draw gametime batch)
+            batch.End()
+        | None -> ()
+        scene
+
 
 
 
