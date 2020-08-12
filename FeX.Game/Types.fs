6630952adfce81bed7e91fff04bac0d0cc1d7a92
Angel D. Munoz <scythes.lol@gmail.com>
2020-08-11 18:21:52 -0600

    add GameObject interface

---
+++ FeX.Game/Types.fs
@@ -0,0 +1,16 @@
+﻿namespace FeX.Game
+
+open System
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Content
+
+module Types =
+
+    type GameObject =
+        inherit IDisposable
+
+        abstract Load : ContentManager -> GameObject
+        abstract Update : GameTime -> GameObject
+        abstract Draw : GameTime -> SpriteBatch -> unit
+
